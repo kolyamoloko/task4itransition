7cedcc163d1794e52ce7edb38a11bcd121f76e89
Mikalai <45792926+kolyamoloko@users.noreply.github.com>
2022-11-16 06:33:51 +0400

    add: users token

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
+const router = require('./router/index')
 
 
 const PORT = process.env.PORT || 5000;
---
+++ server/models/user-model.js
@@ -0,0 +1,13 @@
+const {Schema, model} = require('mongoose');
+
+const UserSchema = new Schema({
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    isActive: {type: Boolean, default: false},
+    status: {type: Boolean},
+    dateRegistration: {type: Date},
+    dateLastConnection: {type: Date},
+    userBlock: {type: Boolean, default: false},
+})
+
+module.exports = model('User', UserSchema);
