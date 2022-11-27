4a2578b327337da562886ace4a6c67f8345ed73b
Mikalai <45792926+kolyamoloko@users.noreply.github.com>
2022-11-27 18:25:10 +0400

    add:new file

--- server/models/token-model.js
+++ server/models/token-model.js
@@ -3,7 +3,6 @@ const {Schema, model} = require('mongoose');
 const TokenShema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: 'User'},
     refreshToken: {type: String, required: true},
-
 })
 
 module.exports = model('Token', TokenShema);
